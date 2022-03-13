022dc31e49c6f589b73a46312aa673e47b9a4f61
kim dogyun <skyehrbs@naver.com>
2022-03-13 16:25:08 +0900

    badge

---
+++ js/main.js
@@ -0,0 +1,33 @@
+const searchEl = document.querySelector('.search');
+const searchInputEl = searchEl.querySelector('input')
+
+searchEl.addEventListener('click', function() {
+  searchInputEl.focus();
+});
+
+searchInputEl.addEventListener('focus', function() {
+  searchEl.classList.add('focused');
+  searchInputEl.setAttribute('placeholder', '통합검색');
+});
+
+searchInputEl.addEventListener('blur', function() {
+  searchEl.classList.remove('focused');
+  searchInputEl.setAttribute('placeholder', '');
+});
+
+const badgeEl = document.querySelector('header .badges');
+
+window.addEventListener('scroll', _.throttle(function () {
+  console.log(window.scrollY);
+  if (window.scrollY > 500) {
+    gsap.to(badgeEl, .6, {
+      opacity: 0,
+      display: 'none'
+    });
+  } else {
+    gsap.to(badgeEl, .6, {
+      opacity: 1,
+      display: 'block'
+    })
+  }
+}, 300));
